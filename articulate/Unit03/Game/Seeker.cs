5e39a57ad483620e1245a6d6762f46b8c53bbc65
lemosjuliana <lemos.julianajesus@gmail.com>
2022-10-12 12:08:42 -0300

    commit

--- articulate/Unit03/Game/Seeker.cs
+++ articulate/Unit03/Game/Seeker.cs
@@ -14,13 +14,21 @@ namespace Unit03.Game
         /// </para>
         /// </summary>
 
+        public class Seeker
+        {
+            private int _location = 0;
+        
 
     // 2) Create the class constructor. Use the following method comment.
         
         /// <summary>
         /// Constructs a new instance of Seeker.
         /// </summary>
-       
+       public Seeker()
+       {
+        Random random = new Random();
+        _location = random.Next(1001);
+       }
 
     // 3) Create the GetLocation() method. Use the following method comment.
         
@@ -29,6 +37,10 @@ namespace Unit03.Game
         /// </summary>
         /// <returns>The current location as an int.</returns>
         
+        public int GetLocation()
+        {
+            return _location;
+        }
 
     // 4) Create the MoveLocation(int location) method. Use the following method comment.
         
@@ -36,5 +48,11 @@ namespace Unit03.Game
         /// Moves to the given location.
         /// </summary>
         /// <param name="location">The given location.</param>
-    
+
+        public void MoveLocation(int location)
+        {
+            this._location = location;
+        }
+
+        }
 }
